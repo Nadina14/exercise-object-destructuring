fbbf598b3eb9b3a50a0e0af84c250d32b7a76b9a
Nadina <sheriefnadina@gmail.com>
2023-11-14 14:24:11 +0100

    feat/ es-2

--- es-2/main.js
+++ es-2/main.js
@@ -1,5 +1,12 @@
 /* Analisi di JSON
 
-Converti la seguente stringa JSON in un oggetto JavaScript e registra l'`email` e l'`id` nella console.
+Converti la seguente stringa JSON in un 
+oggetto JavaScript e registra l'`email` 
+e l'`id` nella console.
 
 '{"id": 1, "nome": "John Doe", "email": "john.doe@example.com"}' */
+
+const stringaJSON = {"id": 1, "nome": "John Doe", "email": "john.doe@example.com"};
+const oggetto = JSON.parse(stringaJSON);
+const {id, email} = oggetto;
+console.log(id, email);
