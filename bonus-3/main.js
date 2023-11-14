38c659b83bf62175216b06c8a0cca075e5f50efd
Nadina <sheriefnadina@gmail.com>
2023-11-14 15:35:19 +0100

    feat/ bonus3-boilerplate

---
+++ bonus-3/main.js
@@ -0,0 +1,20 @@
+/* ### BONUS 3: Distrutturazione ed Elaborazione dei Dati JSON
+
+Hai ricevuto una stringa JSON contenente un array di prodotti con i loro prezzi e quantità. Scrivi una funzione che analizza il JSON e calcola il valore totale di ogni prodotto (prezzo moltiplicato per la quantità). Utilizza la distrutturazione degli oggetti per estrarre le proprietà necessarie. Quindi, registra il nome del prodotto e il suo valore totale.
+[
+    {
+        "product": "Laptop",
+        "price": 1200,
+        "quantity": 5
+    },
+    {
+        "product": "Telefono",
+        "price": 500,
+        "quantity": 10
+    },
+    {
+        "product": "Tablet",
+        "price": 750,
+        "quantity": 3
+    }
+] */ 
