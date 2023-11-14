0b5465986170c584bf020f02be9493ee91dddd71
Nadina <sheriefnadina@gmail.com>
2023-11-14 14:54:38 +0100

    feat/ es-6

--- es-6/main.js
+++ es-6/main.js
@@ -1,8 +1,20 @@
 /* Distrutturazione dei Parametri delle Funzioni
 
-Scrivi una funzione `presentati` che prende un oggetto con le proprietà `nome` e `occupazione` e registra un messaggio nella console.
+Scrivi una funzione `presentati` che prende un 
+oggetto con le proprietà `nome` e `occupazione` 
+e registra un messaggio nella console.
 
 const persona = {
     nome: "Jane",
     occupazione: "sviluppatrice"
 }; */
+
+const persona = {
+    nome: "Jane",
+    occupazione: "sviluppatrice"
+};
+const {nome, occupazione} = persona;
+const presentati = () => {
+    console.log(`Ciao, sono ${nome} e sono una ${occupazione}`);
+}
+presentati(persona);
