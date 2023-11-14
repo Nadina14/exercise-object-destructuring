c89f02d3b1b8cf7edf936981185da6bfa5604935
Nadina <sheriefnadina@gmail.com>
2023-11-14 14:50:28 +0100

    feat/ es-5

--- es-5/main.js
+++ es-5/main.js
@@ -1,7 +1,16 @@
 /* Valori Predefiniti nella Distrutturazione
 
-Utilizza la distrutturazione degli oggetti per estrarre il `nome` e l'`eta` dall'oggetto `persona`. Se l'`eta` non è fornita, dovrebbe avere un valore predefinito di `30`.
+Utilizza la distrutturazione degli oggetti 
+per estrarre il `nome` e l'`eta` dall'oggetto 
+`persona`. Se l'`eta` non è fornita, dovrebbe 
+avere un valore predefinito di `30`.
 
 const persona = {
     nome: "Bob"
 }; */
+
+const persona = {
+    nome: "Bob"
+};
+
+const{nome, eta = 30} = persona;
