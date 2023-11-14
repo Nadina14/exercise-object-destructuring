ff6054a83a993625171ad66e7287d1c5dd972cf1
Nadina <sheriefnadina@gmail.com>
2023-11-14 14:30:33 +0100

    feat/ es-3

--- es-3/main.js
+++ es-3/main.js
@@ -1,6 +1,8 @@
 /* Distrutturazione di Base degli Oggetti
 
-Utilizza la distrutturazione degli oggetti per estrarre il `titolo` e l'`anno` dal seguente oggetto e registrarli nella console.
+Utilizza la distrutturazione degli oggetti 
+per estrarre il `titolo` e l'`anno` dal seguente 
+oggetto e registrarli nella console.
 
 const film = {
     titolo: "Inception",
@@ -8,3 +10,13 @@ const film = {
     anno: 2010,
     valutazione: 8.8
 }; */
+
+const film = {
+    titolo: "Inception",
+    regista: "Christopher Nolan",
+    anno: 2010,
+    valutazione: 8.8
+};
+
+const {titolo, anno} = film;
+console.log(titolo, film)
